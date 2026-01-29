0dd04711a43075e7414d579cfb86bbdd58b099ed
Ophélie Metz <opheliemetz@Mac.lan>
2026-01-29 21:53:14 +0100

    sauv 29/01

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
-const express = require("express");
-const app = express();
-const axios = require("axios");
-const { log } = require("console");
+require("dotenv").config();
 
-app.get("/", (req, res) => {
-  res.json();
+const express = require("express");
+const cors = require("cors");
+const axios = require("axios");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const api = process.env.API_KEY;
+
+app.get("/", async (req, res) => {
+  try {
+    res.status(200).json({ test: "working" });
+  } catch (error) {
+    console.log(error, "error");
+  }
 });
 
-app.listen(3000, () => {
+app.get("/characters", async (req, res) => {
+  try {
+    const response = await axios.get(
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${api}`,
+    );
+    const newTest = req.query.response;
+
+    if (response) {
+      res.status(200).json({ test: newTest });
+    } else {
+      res.status(400).json({ test: "fonctionne pas :(" });
+    }
+  } catch (error) {
+    console.log(error, "error");
+  }
+});
+
+app.all(/.*/, (req, res) => {
+  res.status(404).send("Page introuvable");
+});
+
+const port = process.env.PORT;
+
+app.listen(port, () => {
   console.log("servor has started");
 });
